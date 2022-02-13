063a8d1fea3f81cac36fed310f88ad71f9712b8c
Rayna Weeden <weeden.rayna@outlook.com>
2022-02-12 23:02:15 -0500

    Major upgrade to the menu design

--- js/main.js
+++ js/main.js
@@ -1,28 +1,59 @@
 'use strict';
 // Varables
+var sideNavStatus = 0;
+console.log(sideNavStatus)
+
 
 
 // Constants
-
-
+const sideMenu = document.getElementById("sideMenu");
+const sideMenuInnerContent = document.getElementById("sideMenuInnerContent");
+const bar = document.querySelector('.menuBar');
+const barOne = document.querySelector('.bar-1');
+const barTwo = document.querySelector('.bar-2');
+const barThree = document.querySelector('.bar-3');
+const barFour = document.querySelector('.bar-4');
 
 
 // Functions
 
-function OpenPoemsNav() {
+function sideNav() {
   var screenwidth = $(window).width();
-  if (screenwidth < 720) {
-    document.getElementById("mainPoemsMenuM").style.width = "100%";
-    document.getElementById("mainPoemsContentM").style.opacity = 1;
-  } else {
-    document.getElementById("mainPoemsMenuM").style.width = "40%";
-    document.getElementById("mainPoemsContentM").style.opacity = 1;
+  if (sideNavStatus == 1) {
+    bar.style.transform = "";
+    barOne.style.transform = "";
+    barTwo.style.transform = "";
+    barThree.style.transform = "";
+    barFour.style.transform = "";
+    sideMenu.style.width = "5%";
+    sideMenuInnerContent.style.opacity = "0"
+    window.sideNavStatus = 0;
+    console.log(sideNavStatus)
+    return;
   }
-
-}
-
-function exitPoemsMenu() {
-  document.getElementById("mainPoemsMenuM").style.width = "0%";
+  if (screenwidth < 720 && sideNavStatus == 0) {
+    bar.style.transform = "translate(-5px)";
+    barOne.style.transform = "rotate(45deg) translate(12.5px, 5px)";
+    barTwo.style.transform = "translate(5px)";
+    barThree.style.transform = "rotate(-45deg) translate(12.5px, -5px)";
+    barFour.style.transform = "rotate(90deg) translate(-23px, -5px)";
+    sideMenu.style.width = "100%";
+    sideMenuInnerContent.style.opacity = "1";
+    window.sideNavStatus = 1;
+    console.log(sideNavStatus)
+    return;
+  }
+  if (screenwidth > 720 && sideNavStatus == 0) {
+    bar.style.transform = "translate(-5px)";
+    barOne.style.transform = "rotate(45deg) translate(12.5px, 5px)";
+    barTwo.style.transform = "translate(5px)";
+    barThree.style.transform = "rotate(-45deg) translate(12.5px, -5px)";
+    barFour.style.transform = "rotate(90deg) translate(-23px, -5px)";
+    sideMenuInnerContent.style.opacity = "1";
+    window.sideNavStatus = 1;
+    console.log(sideNavStatus)
+    return;
+  }
 }
 
 // Event Listeners
